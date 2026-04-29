be2632e4e3cefe883325d303f0a63e05c1634343
Loba Alarape <x00183150@mytudublin.ie>
2026-04-29 23:29:41 +0100

    Habit Api

---
+++ HabitTrackApi/Controllers/HabitLogsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HabitTrackerAPI.Data;
+using HabitTrackerAPI.Models;
+
+namespace HabitTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HabitLogsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public HabitLogsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HabitLog>>> GetLogs()
+        {
+            return await _context.HabitLogs.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<HabitLog>> CreateLog(HabitLog log)
+        {
+            _context.HabitLogs.Add(log);
+            await _context.SaveChangesAsync();
+            return log;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLog(int id, HabitLog updatedLog)
+        {
+            var log = await _context.HabitLogs.FindAsync(id);
+            if (log == null) return NotFound();
+
+            log.Completed = updatedLog.Completed;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
